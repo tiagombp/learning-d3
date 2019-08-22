ec723aa7644e8ebb6e6a19269abc6bec433b7453
tiagombp <tiagombp@gmail.com>
2019-08-22 13:39:50 -0300

    inicio do processo de tornar o gráfico responsivo

--- russel-live-coding/grafico.js
+++ russel-live-coding/grafico.js
@@ -6,10 +6,34 @@ const $grafico = d3.select('.grafico-d3');
 const $grafico_interior = d3.select('.grafico-d3-inner');
 const $svg     = $grafico.select('.grafico-d3-svg');
 
+///////////// início coisas que têm a ver com dimensões, que foram tiradas de dentro da função
+
 const ALTURA_BARRA = 30;
 const PADDING_BARRA = ALTURA_BARRA / 2;
 
-// Definir uma função dentro da qual vai ficar a criação
+// captura largura do div que envolve o svg
+const w = $grafico_interior.node().offsetWidth;
+const h = (ALTURA_BARRA + PADDING_BARRA) * data.length;
+
+console.log("oi", {w, h});
+
+//////////// fim das coisas.
+
+// Definir uma função de rendering
+function render() {
+    $rect  // firefox não entende "style" para svgs, só 'attr'.
+      .attr('x', 0)
+      .attr('y', (d, i) => (ALTURA_BARRA + PADDING_BARRA) * i)
+      .attr('width', d => scale(d.value))
+      .attr('height', ALTURA_BARRA);
+
+    //console.log($rect);
+    //console.log(d3.selectAll('rect'));
+
+}
+
+
+// Definir uma função dentro da qual vai ficar o DATA JOIN
 
 function init() {
     const data = d3.range(5).map(i => ({
@@ -17,23 +41,23 @@ function init() {
         value: Math.random()
     }));
 
-    console.table(data);
+    //// sort data
+    data.sort((a,b) => d3.descending(a.value, b.value));
+    //console.table(data);
 
-    const w = $grafico_interior.node().offsetWidth;
-    console.log("oi", {w});
+
+    $svg
+       .attr('width', w)
+       .attr('height', h);
+
+
+    const scale = d3.scaleLinear().range([0, w]);
 
     const $rect = $svg
       .selectAll('rect')
       .data(data)
       .enter()
       .append('rect');
-
-    $rect
-      .style('x', d => d.value);
-
-    console.log($rect);
-
-    console.log(d3.selectAll('rect'));
 }
 
 init();
