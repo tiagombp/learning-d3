1a741f02ecb043cbc9f1f31f0dee5aaa3a7c1dd8
tiagombp <tiagombp@gmail.com>
2019-08-22 17:32:47 -0300

    finaliza e inclui função debounce para dar um atraso na chamada do resize

--- russel-live-coding/grafico.js
+++ russel-live-coding/grafico.js
@@ -1,3 +1,20 @@
+// função debounce para dar um atraso na chamada do resize
+// https://davidwalsh.name/function-debounce
+const debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 // Russell uses the following convention:
 // $aaa : variables that grab references to DOM elements;
 // CAPS : constants
@@ -11,29 +28,53 @@ const $svg     = $grafico.select('.grafico-d3-svg');
 const ALTURA_BARRA = 30;
 const PADDING_BARRA = ALTURA_BARRA / 2;
 
-// captura largura do div que envolve o svg
-const w = $grafico_interior.node().offsetWidth;
-const h = (ALTURA_BARRA + PADDING_BARRA) * data.length;
 
-console.log("oi", {w, h});
+//const h = (ALTURA_BARRA + PADDING_BARRA) * data.length;
 
-//////////// fim das coisas.
+//////////// fim das coisas dimensionais.
+
+// Definir uma função de resize
+
+function resize() {
+
+    console.log('resize'); // só para mostrar que está funcionando.
+
+    // captura largura do div que envolve o svg
+    const w = $grafico_interior.node().offsetWidth;
+
+    // agora chama o render toda vez que mudar o tamanho, passando a largura:
+    render(w);
+
+}
 
 // Definir uma função de rendering
-function render() {
-    $rect  // firefox não entende "style" para svgs, só 'attr'.
+// Vai de fato desenhar o gráfico. Mas precisa saber o scale, por exemplo.
+
+function render(w) {
+    const $rect = $svg.selectAll('rect');
+    const h = (ALTURA_BARRA + PADDING_BARRA) * $rect.size();
+    // console.log(data.length)
+    // em vez de fazer um data.length aqui, ele faz um $rect.size() para obter a quantidade de retângulos
+    // pq esta função, que é chamada por uma função que é chamada por uma função em que `data` foi definida,
+    // não enxerga essa variável? 
+
+    $svg
+      .attr('width' , w)
+      .attr('height', h);
+
+    // a escala vai ser atualizada toda vez que a função for chamada, o que vai acontecer toda vez que a largura for alterada.
+    const scale = d3.scaleLinear().range([0, w]);
+
+    // firefox não entende "style" para svgs, só 'attr'.
+    $rect
       .attr('x', 0)
       .attr('y', (d, i) => (ALTURA_BARRA + PADDING_BARRA) * i)
       .attr('width', d => scale(d.value))
       .attr('height', ALTURA_BARRA);
-
-    //console.log($rect);
-    //console.log(d3.selectAll('rect'));
-
 }
 
-
-// Definir uma função dentro da qual vai ficar o DATA JOIN
+// Definir uma função dentro da qual vai ficar o DATA JOIN:
+// Vai pegar o data, fazer o bind e criar os elementos.
 
 function init() {
     const data = d3.range(5).map(i => ({
@@ -45,19 +86,16 @@ function init() {
     data.sort((a,b) => d3.descending(a.value, b.value));
     //console.table(data);
 
-
-    $svg
-       .attr('width', w)
-       .attr('height', h);
-
-
-    const scale = d3.scaleLinear().range([0, w]);
-
-    const $rect = $svg
+    $svg // Russel não inicializa mais uma variável para pegar essa seleção... const $rect = $svg.selectAll... 
       .selectAll('rect')
       .data(data)
       .enter()
       .append('rect');
+
+
+    // acrescenta um resize listener
+    window.addEventListener('resize', debounce(resize, 500));
+    resize(); // a primeira chamada, porque no início não tem evento de resize
 }
 
 init();
